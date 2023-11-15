ff9668e64f1f33dafc33b67a1f79ca92f545effe
Mund3k <74401169+kubat1611@users.noreply.github.com>
2023-11-15 18:26:36 +0100

    initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <h1>Ściąga Biologia</h1>
+        </nav>
+    )
+  }
+}
+
